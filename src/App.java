94e70e74b49c201ee0fdbd23309feb549632b890
emlacuden <hungpp250795@gmail.com>
2018-03-20 17:38:35 +0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public boolean thisCausesFindBugWarnings(String s) {
+        return "abc".equals(s);
+    }
+
+    public void thisCreatesCompilerWarnings() {
+        List a = new ArrayList();
+        a.add("foo");
+    }
+
+    // we'll be testing this method from tests
+    public int add(int x, int y) {
+        return x+y;
+    }
+    public String thisIsABug() {
+    	String str = null;
+    	return str.toUpperCase();
+    }
+}
